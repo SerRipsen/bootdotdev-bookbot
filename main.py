759d7f59cb0638b235cc73ac2036693b3e33476a
Ser Ripsen <ser@ripsen.com>
2025-11-21 18:31:12 +0000

    Added basic functions to main.py for reading one specific book and returning the word count

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def count_words(text):
+    word_list = text.split()
+    return len(word_list)
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    path_to_books = "./books"
+    books = get_book_text(f"{path_to_books}/frankenstein.txt")
+    count = count_words(books)
+    print(count)
+
+main()
